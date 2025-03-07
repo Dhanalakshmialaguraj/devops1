2f49d6a2d615bc43e61cd15b15f79b8fe5c4ee7f
Dhanalakshmialaguraj <rjdhana41107@gmail.com>
2025-03-07 08:54:44 +0530

    Update maintest.java

--- src/test/java/com/example/maintest.java
+++ src/test/java/com/example/maintest.java
@@ -1,1 +1,19 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class maintest {
+    private main main;
+
+    @Before // This runs before each test
+    public void setUp() {
+        main = new main();
+    }
+
+    @Test
+    public void testGreet() {
+        assertEquals("Hello, Jenkins!", main.greet());
+    }
 
