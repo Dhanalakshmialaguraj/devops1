b30da877169d14cab06be22f539675304d8510ce
Dhanalakshmialaguraj <rjdhana41107@gmail.com>
2025-03-07 08:52:39 +0530

    Update main.java

--- src/main/java/com/example/main.java
+++ src/main/java/com/example/main.java
@@ -1,1 +1,12 @@
 
+package com.example;
+
+public class main {
+    public String greet() {
+        return "Hello, Jenkins!"; 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new main().greet()); 
+    }
+}
